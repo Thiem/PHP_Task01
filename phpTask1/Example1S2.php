d9a4e5e8367f4878131f9b11544975f38af61962
Thiem <tranthiem1993@gmail.com>
2021-06-25 21:53:35 +0700

    Task01

---
+++ phpTask1/Example1S2.php
@@ -0,0 +1,12 @@
+<?php
+    // error_reporting(0);
+    $A = $_GET["n1Text"];
+    $B = $_GET["n2Text"];
+    $C = $_GET["n3Text"];
+    $D = $_GET["n4Text"];
+    echo "Your personal details";
+    echo "<br><br>";
+    echo "First Name: $A <br/>";
+    echo "Last Name: $B <br/>";
+    echo "Address: $C <br/>";
+    echo "Contact No: $D <br/>";
---
+++ phpTask1/Example3S1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Syntax Example</title>
+    <?php echo "Hello world!" ?>
+</head>
+<body>
+    <div>
+        The Date is:
+        <?php echo gmdate("M d Y");
+            echo "Hello Everybody!";
+            ?>
+        <hr/>
+        <?php
+            $str = "My name is Thiem";
+            echo $str;
+            header('WWW-Authenticate: Negotiate');
+            header('WWW-Authenticate: NTLM',false);
+//            header("Location: http//google.com.vn", true,303);
+        ?>
+    </div>
+</body>
+</html>
